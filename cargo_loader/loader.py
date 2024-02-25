0990349234fac77174cefd03e29de0b056b39d51
Petrik van der Velde <petrikvandervelde@gmail.com>
2024-02-25 19:11:47 +1300

    Define a simple first-in-first-served cargo loader

---
+++ cargo_loader/loader.py
@@ -0,0 +1,57 @@
+from abc import ABC, abstractmethod
+from typing import List
+
+from cargo_loader.cargo import Cargo
+from cargo_loader.trolley import TROLLEY_MAXIMUM_CARGO_WEIGHT_IN_KG
+
+# Provides different loader algorithms to load cargo items into a vehicle.
+
+
+class Loader(ABC):
+    #
+    # Define the loader as an abstract class so that we can define different loader algorithms.
+    #
+    # Because the loader doesn't need to store any state beyond the state kept during the 'load'
+    # method this could also have been done with a function definition (i.e. having multiple
+    # functions with the same signature but different implementations).
+    # Because python doesn't have a good way to define function signatures using functions would
+    # be less clear than using an abstract class.
+    #
+    # The loader is trying to 'solve' the [bin packing problem](https://en.wikipedia.org/wiki/Bin_packing_problem).
+
+    @abstractmethod
+    def load(self, cargo_items: List[Cargo]) -> int:
+        #
+        # Load the cargo items into one or more cargo trolleys and return the number of trolleys that were loaded.
+        #
+
+        # Return an invalid number so that it is clear that the method needs to be implemented.
+        return -1
+
+class FirstFitLoader(Loader):
+    #
+    # A simple loader algorithm that tries to load as many items as possible into a trolley.
+    #
+    # The algorithm works by trying to load the items into the first trolley that can fit the item.
+    # If no trolley can fit the item a new trolley is used. This algorithm is simple and fast but doesn't always produce
+    # the best results.
+    #
+
+    def load(self, cargo_items: List[Cargo]) -> int:
+        #
+        # Load the cargo items into one or more cargo trolleys and return the number of trolleys that were loaded.
+        #
+
+        # Load the cargo items into trolleys
+        current_trolley_weight = 0
+        trolley_count = 1
+        for cargo in cargo_items:
+            # Try to load the cargo into an existing trolley
+            if current_trolley_weight + cargo.weight_in_kg <= TROLLEY_MAXIMUM_CARGO_WEIGHT_IN_KG:
+                current_trolley_weight += cargo.weight_in_kg
+            else:
+                # If the cargo couldn't be loaded into an existing trolley create a new trolley
+                trolley_count += 1
+                current_trolley_weight = cargo.weight_in_kg
+
+        return trolley_count
